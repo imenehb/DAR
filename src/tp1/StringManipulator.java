1f152a74cdf6e139b162e9f9a6a65245859e05bf
imenehb <79918748+imenehb@users.noreply.github.com>
2021-10-20 19:16:11 +0100

    Update StringManipulator.java

--- src/tp1/StringManipulator.java
+++ src/tp1/StringManipulator.java
@@ -3,34 +3,63 @@ package tp1;
 public class StringManipulator {
 
     private String s;
+	private String t="";
+	private char ch;
+	private int vCount = 0;
+	boolean b;
 
     public StringManipulator(String s) {
         this.s = s;
     }
 
     public void reverse(){
-        //do your stuff
-        System.out.println("reverse : ");
+        for (int i=0; i<s.length(); i++)
+	        {
+	          ch= s.charAt(i); 
+	          t= ch+t; 
+	        }
+	    System.out.println("reverse : "+t);
     }
 
     public void isPalindrome(){
-        //do your stuff
-        System.out.println("isPalindrome : ");
+        b=true;
+	    	int i = 0;
+	    	int j = s.length()-1;
+	    	 
+	    	s = s.toLowerCase();
+	    	
+	        while (i < j) {
+	            if (s.charAt(i) != s.charAt(j))
+	                b=false;
+	            i++;
+	            j--;
+	            
+	        }
+	        
+	        if (b==true)
+	        	System.out.println("palindrome : oui");
+	        else
+	        	System.out.println("palindrome : non");
     }
 
     public void toUpperCase(){
-        //do your stuff
-        System.out.println("uppercase : ");
+        t=s.toUpperCase();
+	        System.out.println("uppercase : "+t);
     }
 
     public void toLowerCase(){
-        //do your stuff
-        System.out.println("lowercase : ");
+        t=s.toLowerCase();
+	        System.out.println("lowercase : "+t);
     }
 
     public void getVowelNumber(){
-        //do your stuff
-        System.out.println("vowelNumber : ");
+        s = s.toLowerCase();    
+	        for(int i = 0; i < s.length(); i++) {  
+	            if(s.charAt(i) == 'a' || s.charAt(i) == 'e' || s.charAt(i) == 'i' || s.charAt(i) == 'o' || s.charAt(i) == 'u') {    
+	                vCount++;    
+	            }
+	        }  
+	        System.out.println("vowelNumber : "+vCount);
     }
 
     public static void main(String[] args) {
