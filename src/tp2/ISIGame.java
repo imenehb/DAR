772e0a5fa4a28aa9f13996d93a3c67f880bde9bb
imenehb <79918748+imenehb@users.noreply.github.com>
2021-10-24 11:10:14 +0100

    Update ISIGame.java

--- src/tp2/ISIGame.java
+++ src/tp2/ISIGame.java
@@ -1,24 +1,43 @@
 package tp2;
 
 import java.util.Scanner;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
 
-public class ISIGame {
+public class main {
 
-    static class ISIException extends RuntimeException{
-        public ISIException(String s) {
-            super(s);
-        }
-    }
+    //Now we want to create a program that takes a file name and prints it s content
 
     public static void main(String[] args) {
-        System.out.println("Enter a word");
-
+        System.out.println("Give me your file name :)"); //try isi as an input ;)
         Scanner scanner = new Scanner(System.in);
-        String line = scanner.nextLine();
+        String fileName = scanner.nextLine();
+        printFileContent(fileName);
+        scanner.close();
+    }
 
-        // now you have to check if the word contains 'isi' or not
-        // if not print 'ok'
-        // if yes throw the exception 'ISIException'
-        // and when you catch it print 'life is not isi'
+    //Here you have 2 methods
+    // getFileContent aims to get the content of a file
+    // printFileContent that prints it s content otherwise print 'file not found :/'
+    // todo : implement the methods and force exception handling in the printFile method
+
+    private static String getFileContent(String fileName) {
+        File file = new File("src/exp3/" + fileName);
+        //the lines below help you to get the content
+       try {
+        Scanner reader = new Scanner(file);
+        String content = reader.nextLine();
+        return content;
+       } catch (FileNotFoundException e) {
+           //TODO: handle exception
+           return "file not found" ;
+       }
+        
+    }
+
+    private static void printFileContent(String fileName) {
+        String data = getFileContent(fileName);
+        System.out.println(data);
     }
 }
