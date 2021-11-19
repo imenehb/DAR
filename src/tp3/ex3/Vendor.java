05ad75722b4c7a560fd27465d1a3676a0ec112ac
imenehb <79918748+imenehb@users.noreply.github.com>
2021-11-19 19:43:12 +0100

    Update Vendor.java

--- src/tp3/ex3/Vendor.java
+++ src/tp3/ex3/Vendor.java
@@ -14,7 +14,9 @@ public class Vendor extends Thread{
         try {
             sleep(2000);
                 System.out.println("Vendor "+this.getName()+" said 'isi sandwich is ready'");
-                //todo : prepare a sandwich, set it as ready and notify a student
+                synchronized (isiSandwich){
+                isiSandwich.notifyAll();
+            }
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
